a90f0bc189d301564ef32e3613b01e1ab511d362
myxsan <yksl3461@gmail.com>
2022-12-22 02:55:17 +0300

    Learned how to use let and const also var

---
+++ js/vars.js
@@ -0,0 +1,22 @@
+// Using let and const to define variables
+
+// Defining a variable with var (old)
+// var serverName = "example.com";
+// console.log(serverName); // "example.com"
+
+// Using let to define variables
+// let serverName;
+// console.log(serverName); //tried to use null variable with let
+// let serverName = "example.com";
+// console.log(serverName); //Using let to define variable
+// Changing the value of the variable
+// let fullName = "John Smith";
+// fullName = "John";
+// console.log(fullName); // "John"
+
+//Using const to define variables
+// const SERVER_PASSWORD; --> Const variables can not be null and changed!!!
+// const SERVER_PASSWORD = "password";
+// console.log(SERVER_PASSWORD)
+// SERVER_PASSWORD = "Consts can not be null and changed"; --> Const variables can not be null and changed!!! It throws an error
+
