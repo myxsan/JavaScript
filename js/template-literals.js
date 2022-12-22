d92fb663978f730dbdc263d319886964b1cea02e
myxsan <yksl3461@gmail.com>
2022-12-23 02:34:16 +0300

    template literals

---
+++ js/template-literals.js
@@ -0,0 +1,11 @@
+//collapsing strings
+/*
+let username = "name"
+const domain = "mail.com"
+let email = name + "@" + domain
+info = `Welcome to this site ${username}, your email is ${email}
+
+length of your email is ${email.length}
+year is ${new Date().getFullYear()}`
+console.log(info)
+*/
--- js/types.js
+++ js/types.js
@@ -9,19 +9,20 @@ console.log("str: ",typeof(str))
 console.log("bool: ",typeof(bool))
 */
 
+/*
 //Converting string to int and float
 //Wirh Number() you can convert string to int or float but there can't be any string characters
 let num = "111"
 let wrong = "11px"
 console.log(Number(num))
 console.log(Number(wrong)) //NaN
+*/
 
+/*
 let int = "111px"
 let float = "1.11px"
 //with parseInt() and parseFloat() you can convert string to int even there are anumeretic characters
 
 console.log(parseInt(int))
 console.log(parseFloat(float))
-
-
-
+*/
