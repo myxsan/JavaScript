f9e93d00c12e3c749f8361a9e89751fc9713e668
myxsan <yksl3461@gmail.com>
2022-12-23 01:37:25 +0300

    Used numbers

---
+++ js/numbers.js
@@ -0,0 +1,58 @@
+/*
+//Using Numbers
+let price = 100
+let tax = 0.18
+let taxPrice = price * tax
+let totalPrice = price + tax
+
+console.log("price: ", price," tax: ", tax," Tax Price: ", taxPrice, " Total Price: ", totalPrice)
+*/
+
+/*
+//Incresing and Decresing numbers
+let counter = 315
+counter = counter + 1
+counter += 1
+counter++
+
+counter = counter - 1
+counter -= 1
+counter--
+
+counter *= 10
+counter /= 10
+*/
+/*
+//Opreation order
+
+// () > * >= / > + >= -
+
+console.log((10 + 5) * 2 + 10 / 2) //(10 + 5) * 2 + 10 / 2 = 6
+*/
+
+/*
+//Module
+test = 10 % 3 // 1
+*/
+
+/*
+//exponentional
+exp = 10 ** 2 // 100
+*/
+
+/*
+ceil = Math.ceil(10.25) // 11
+console.log("ceil: ", ceil)
+floor = Math.floor(10.25) // 10
+console.log("floor: ", floor)
+rounded = Math.round(10.25) // 10 *if > 5 up else down
+console.log("rounded: ", rounded)
+*/
+
+/*
+//converting string to number
+stringNum = "10"
+num = Number(stringNum)
+console.log("stringNum: ", stringNum)
+console.log("num: ", num)
+*/
