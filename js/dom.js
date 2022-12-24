7825eb43c7e645c3f05b40853c062c41c8bda0ef
myxsan <yksl3461@gmail.com>
2022-12-24 21:04:52 +0300

    DOM

---
+++ js/dom.js
@@ -0,0 +1,37 @@
+/*
+//URL
+console.log(document.URL);
+//pathname
+console.log(document.pathname);
+*/
+
+// console.log(document.head);
+// console.log(document.body);
+
+// document.body.style.backgroundColor = "red";
+
+// let title = document.getElementsByTagName('h1')
+// console.log(title)
+
+// let title = document.getElementById('title')
+// console.log(title)
+// console.log(title.innerHTML)
+// title.innerHTML = "asd"
+
+// let link = document.querySelector('ul#link>li>a')
+// link.innerHTML = "Changed link"
+// console.log(link.innerHTML)
+// link.style.color = "red"
+
+// let firstChild = document.querySelector('ul#list>li:first-child')
+// let lastChild = document.querySelector('ul#list>li:last-child')
+// console.log(firstChild)
+// console.log(lastChild)
+
+// let ulDOM = document.querySelector('ul#list')
+// let liDOM = document.createElement('li')
+
+// liDOM.innerHTML = "Created li element"
+
+// ulDOM.append(liDOM) //-> last
+// ulDOM.prepend(liDOM) //-> first
