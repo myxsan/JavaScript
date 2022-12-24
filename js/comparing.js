26de6d04a61a1f8c52fd1fd12fcc549ac434eda8
myxsan <yksl3461@gmail.com>
2022-12-24 22:00:09 +0300

    Comparison stractures

---
+++ js/comparing.js
@@ -0,0 +1,20 @@
+// //values euqal
+// 1 == "1"; // -> true
+
+// //values and types equal
+// 1 === "1"; // -> false
+// 1 === 1 // -> true
+
+// //not equal
+// 1 != 1 // -> false
+// 1 != 2 // -> true
+// console.log(!!2)
+// s = 'a'
+// if (['a','e','i','o','u'].includes(s)) {
+//     console.log("bitch")
+// }
+
+// let userName = prompt("Please enter your nick: ")
+// let info = document.querySelector('#title')
+
+// info.innerHTML = `${userName.length > 0 ? userName : "You didn't enter a valid nick"}`
