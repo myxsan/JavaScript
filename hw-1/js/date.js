8141e04c62f31028d4437d3c97e9c987b8637e64
myxsan <yksl3461@gmail.com>
2022-12-25 16:33:36 +0300

    update

--- hw-1/js/date.js
+++ hw-1/js/date.js
@@ -2,7 +2,7 @@ let content = document.querySelector('#date')
 var today = new Date();
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-function timeCount() {
+setTimeout(() => {
     var today = new Date();
 
     var hour = today.getHours();
@@ -15,6 +15,4 @@ function timeCount() {
     if(second<10)second = "0"+second;
 
     document.getElementById("date").innerHTML = `${hour}:${minute}:${second} ${days[today.getDay()]}`;
-
-    setTimeout("timeCount()", 1000);
-}
+}, 1000);
